0230d0a9551282102e00212912d185e04531087d
William Maguire <billymaguire3@gmail.com>
2020-11-03 04:07:27 -0500

    Make the entire entry form component one big form so I can use reset() to clear the form after adding the user and their picks to database

--- client/src/components/EntryForm.jsx
+++ client/src/components/EntryForm.jsx
@@ -38,6 +38,7 @@ class EntryForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     const user = this.state;
+    console.log(user);
     this.props.addUser(user);
     this.setState({
       currentEntry: this.state.entryName,
@@ -45,14 +46,15 @@ class EntryForm extends React.Component {
       userName: '',
       entryName: '',
     });
+    event.target.reset();
   }
 
   render() {
     const { schedule } = this.props;
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <div>
-          <form>
+          <div>
             <label>
               Username:
               <input
@@ -71,77 +73,76 @@ class EntryForm extends React.Component {
                 onChange={this.handleChange}
               />
             </label>
-            <button onClick={this.handleSubmit}>Create Entry</button>
-          </form>
-          <h4>{this.state.currentUser}</h4>
-          <h4>{this.state.currentEntry}</h4>
-        </div>
-        <div>
-          <div>
-            <select>
-              <option value="week 9" onChange={this.handleChange}>Week {this.state.weekNumberValue}</option>
-            </select>
+            {/* <button onClick={this.handleSubmit}>Create Entry</button> */}
           </div>
-          <div className="weekMatchups">
-            <div className="table-container">
-              <table className="main-table">
-                <thead id="table-header">
-                  <tr id="table-header-row">
-                    <th className="date-time">Day/Month/Time</th>
-                    <th className="location">Location</th>
-                    <th className="home-team">Home Team</th>
-                    <th className="away-team">Away Team</th>
-                    <th className="result">Result</th>
-                    <th className="spread">Spread</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    schedule.map((matchup) => {
-                      const dateTime = `${matchup.date.slice(0, 5)} ${matchup.date.slice(-4)}`;
-                      return (
-                        <tr key={matchup._id}>
-                          <td className="date-time">
-                            <div>{dateTime}</div>
-                          </td>
-                          <td className="location">
-                            <div>{matchup.location}</div>
-                          </td>
-                          <td className="home-team">
-                            <div>{matchup.homeTeam}</div>
-                            <input
-                              name="homeTeamSelected"
-                              type="checkbox"
-                              // onClick={this.savePick(matchup.homeTeam)}
-                              onChange={this.handlePickChange.bind(this, matchup.homeTeam)} />
-                          </td>
-                          <td className="away-team">
-                            <div>{matchup.awayTeam}</div>
-                            <input
-                              name="awayTeamSelected"
-                              type="checkbox"
-                              // onClick={this.savePick(matchup.homeTeam)}
-                              onChange={this.handlePickChange.bind(this, matchup.awayTeam)} />
-                          </td>
-                          <td className="result">
-                            <div>{matchup.result}</div>
-                          </td>
-                          <td className="spread">
-                            <div>{matchup.spread}</div>
-                          </td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table>
-              <div id="submit-button">
-                <button>Submit My Picks</button>
+          <div>
+            <div>
+              <select>
+                <option value="week 9" onChange={this.handleChange}>Week {this.state.weekNumberValue}</option>
+              </select>
+            </div>
+            <div className="weekMatchups">
+              <div className="table-container">
+                <table className="main-table">
+                  <thead id="table-header">
+                    <tr id="table-header-row">
+                      <th className="date-time">Day/Month/Time</th>
+                      <th className="location">Location</th>
+                      <th className="home-team">Home Team</th>
+                      <th className="away-team">Away Team</th>
+                      <th className="result">Result</th>
+                      <th className="spread">Spread</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      schedule.map((matchup) => {
+                        const dateTime = `${matchup.date.slice(0, 5)} ${matchup.date.slice(-4)}`;
+                        return (
+                          <tr key={matchup._id}>
+                            <td className="date-time">
+                              <div>{dateTime}</div>
+                            </td>
+                            <td className="location">
+                              <div>{matchup.location}</div>
+                            </td>
+                            <td className="home-team">
+                              <div>{matchup.homeTeam}</div>
+                              <input
+                                name="homeTeamSelected"
+                                type="checkbox"
+                                // onClick={this.savePick(matchup.homeTeam)}
+                                onChange={this.handlePickChange.bind(this, matchup.homeTeam)} />
+                            </td>
+                            <td className="away-team">
+                              <div>{matchup.awayTeam}</div>
+                              <input
+                                name="awayTeamSelected"
+                                type="checkbox"
+                                // onClick={this.savePick(matchup.homeTeam)}
+                                onChange={this.handlePickChange.bind(this, matchup.awayTeam)} />
+                            </td>
+                            <td className="result">
+                              <div>{matchup.result}</div>
+                            </td>
+                            <td className="spread">
+                              <div>{matchup.spread}</div>
+                            </td>
+                          </tr>
+                        );
+                      })
+                    }
+                  </tbody>
+                </table>
+                <div id="submit-button">
+                  {/* <button onClick={this.handleSubmit}>Submit My Picks</button> */}
+                  <input type="submit" value="Submit My Sitch" />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
 
     );
   }
--- database/models/users.js
+++ database/models/users.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const usersSchema = new mongoose.Schema({
   userName: String,
   entryName: String,
+  picks: String,
 });
 
 const User = mongoose.model('Users', usersSchema);
