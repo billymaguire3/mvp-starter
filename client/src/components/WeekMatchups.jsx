fadc4b18ec43ea15b8259c20b9f368b6d724fe47
William Maguire <billymaguire3@gmail.com>
2020-11-02 22:36:43 -0500

    Render week 9 matchups to page using table

--- client/src/components/WeekMatchups.jsx
+++ client/src/components/WeekMatchups.jsx
@@ -17,27 +17,50 @@ class WeekMatchups extends React.Component {
         <table className="main-table">
           <thead id="table-header">
             <tr id="table-header-row">
-              <th classname="date-time">Date/Time</th>
+              <th classname="date-time">Day/Month/Time</th>
               <th classname="location">Location</th>
               <th classname="home-team">Home Team</th>
               <th classname="away-team">Away Team</th>
               <th classname="result">Result</th>
               <th classname="spread">Spread</th>
-              <th classname="pick">Pick</th>
+              {/* <th classname="pick">Pick</th> */}
             </tr>
           </thead>
+          <tbody>
+            {
+              schedule.map((matchup) => {
+                const dateTime = `${matchup.date.slice(0, 5)} ${matchup.date.slice(-4)}`;
+                return (
+                  <tr>
+                    <td class="date-time">
+                      <div>{dateTime}</div>
+                    </td>
+                    <td className="location">
+                      <div>{matchup.location}</div>
+                    </td>
+                    <td className="home-team">
+                      <div>{matchup.homeTeam}</div>
+                      <input name="homeTeamSelected" type="checkbox" />
+                    </td>
+                    <td className="away-team">
+                      <div>{matchup.awayTeam}</div>
+                      <input name="awayTeamSelected" type="checkbox" />
+                    </td>
+                    <td className="result">
+                      <div>{matchup.result}</div>
+                    </td>
+                    <td className="spread">
+                      <div>{matchup.spread}</div>
+                    </td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
         </table>
-        <ul>
-          {
-            schedule.map((matchup) => {
-              return (
-                <li>
-
-                </li>
-              );
-            })
-          }
-        </ul>
+        <div id="submit-button">
+          <button>Submit My Picks</button>
+        </div>
       </div>
     );
   }
